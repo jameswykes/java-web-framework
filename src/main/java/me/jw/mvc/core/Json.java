7213980e36d639f4aa342db080e8c3e5edf1f56a
James Wykes <jamesrw@gmail.com>
2014-04-20 17:13:27 +0100

    UPDATE lazy initialise Gson and use single instance

--- src/main/java/me/jw/mvc/core/Json.java
+++ src/main/java/me/jw/mvc/core/Json.java
@@ -2,16 +2,19 @@ package me.jw.mvc.core;
 
 import com.google.gson.Gson;
 
-public class Json extends Action { 
-	private Object object;
+public class Json extends Action {
+    static Gson g;
+    private Object object;
 
-	public Json (Object object) {
-		this.object = object;
-	}
+    public Json(Object object) {
+        if (g == null) {
+            g = new Gson();
+        }
+        this.object = object;
+    }
 
-	@Override
-	public void prepare () {
-		Gson g = new Gson ();
-		setOutput (g.toJson(object));
-	}
+    @Override
+    public void prepare() {
+        setOutput(g.toJson(object));
+    }
 }
